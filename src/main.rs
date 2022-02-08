1defe4e579af014a24589700329026219072aa52
Icelk <main@icelk.dev>
2022-02-08 13:33:54 +0100

    Made prompt only appear when a tty is connected. Also greatly improved performance.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use std::collections::HashMap;
 use std::io::{stdin, stdout, BufRead, Write};
 use std::process::exit;
 use std::str::FromStr;
 use std::time::Instant;
+use std::{env, hash};
 
 fn f() {
     stdout().lock().flush().unwrap();
@@ -140,16 +142,55 @@ fn parse<T: FromStr>(s: &str) -> Option<T> {
     }
 }
 
+#[derive(Debug, Copy, Clone)]
+struct F64Hash(f64);
+
+impl F64Hash {
+    fn key(&self) -> u64 {
+        self.0.to_bits()
+    }
+}
+
+impl hash::Hash for F64Hash {
+    fn hash<H>(&self, state: &mut H)
+    where
+        H: hash::Hasher,
+    {
+        self.key().hash(state)
+    }
+}
+
+impl PartialEq for F64Hash {
+    fn eq(&self, other: &F64Hash) -> bool {
+        self.key() == other.key()
+    }
+}
+
+impl Eq for F64Hash {}
+
 fn main() {
+    let performance_print = env::var("DEBUG_PERFORMANCE")
+        .ok()
+        .map_or(false, |s| !s.trim().is_empty());
+
+    let tty = atty::is(atty::Stream::Stdin);
+
     loop {
-        print!("> ");
-        f();
+        if tty {
+            print!("> ");
+            f();
+        }
         let mut s = String::new();
+
         stdin().lock().read_line(&mut s).unwrap();
+
+        if s.trim().is_empty() {
+            exit(0);
+        }
 
         let now = Instant::now();
 
-        let mut values: Vec<_> = s
+        let values: Vec<_> = s
             .split(',')
             .map(|s| s.split_whitespace())
             .flatten()
@@ -164,27 +205,44 @@ fn main() {
             .collect();
 
         if values.is_empty() {
-            eprintln!("\nNo input. Exiting.");
-            exit(0);
+            eprintln!("Only invalid input. Try again.");
+            continue;
         }
 
-        println!("Parsing took {}µs", now.elapsed().as_micros());
+        if performance_print {
+            println!("Parsing took {}µs", now.elapsed().as_micros());
+        }
+        let now = Instant::now();
+
+        let mut collected = HashMap::with_capacity(16);
+        for (v, count) in &values {
+            let c = collected.entry(F64Hash(*v)).or_insert(0);
+            *c += count;
+        }
+        let mut values: Vec<_> = collected.into_iter().map(|(f, c)| (f.0, c)).collect();
+
+        if performance_print {
+            println!("Optimizing input took {}µs", now.elapsed().as_micros());
+        }
         let now = Instant::now();
 
         let out = std_dev(MultipleList::new(&values));
 
-        println!(
-            "Standard deviation & mean took {}µs",
-            now.elapsed().as_micros()
-        );
+        if performance_print {
+            println!(
+                "Standard deviation & mean took {}µs",
+                now.elapsed().as_micros()
+            );
+        }
         let now = Instant::now();
 
         values.sort_unstable_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
-        println!("VAlues {values:?}");
 
         let median = median(MultipleList::new(&values));
 
-        println!("Median & quadrilles took {}µs", now.elapsed().as_micros());
+        if performance_print {
+            println!("Median & quadrilles took {}µs", now.elapsed().as_micros());
+        }
 
         println!(
             "Standard deviation: {}, mean: {}, median: {}, lower quadrille: {:?}, higher quadrille: {:?}",
