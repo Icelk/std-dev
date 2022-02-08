9aeeefe57c4e0f4113b4c41f843be616d2c952db
Icelk <main@icelk.dev>
2022-02-08 13:48:20 +0100

    Now usable as library.

---
+++ src/lib.rs
@@ -0,0 +1,126 @@
+/// A list of clusters.
+///
+/// A cluster is a value and the count.
+pub struct ClusterList<'a> {
+    list: &'a [(f64, usize)],
+}
+impl<'a> ClusterList<'a> {
+    /// The float is the value. The integer is the count.
+    pub fn new(list: &'a [(f64, usize)]) -> Self {
+        Self { list }
+    }
+
+    /// O(n)
+    pub fn len(&self) -> usize {
+        self.list.iter().map(|(_, count)| *count).sum()
+    }
+    /// O(1)
+    pub fn is_empty(&self) -> bool {
+        self.list.is_empty()
+    }
+    pub fn sum(&self) -> f64 {
+        let mut sum = 0.0;
+        for (v, count) in self.list.iter() {
+            sum += v * *count as f64;
+        }
+        sum
+    }
+    fn sum_squared_diff(&self, base: f64) -> f64 {
+        let mut sum = 0.0;
+        for (v, count) in self.list.iter() {
+            sum += (v - base).powi(2) * *count as f64;
+        }
+        sum
+    }
+    /// The inner list must be sorted by the `f64`.
+    pub fn median(&self) -> f64 {
+        let len = self.len();
+        let even = len % 2 == 0;
+        let mut len = len;
+        let target = len / 2;
+
+        for (pos, (v, count)) in self.list.iter().enumerate() {
+            len -= *count;
+            if len + 1 == target && even {
+                let mean = (*v + self.list[pos - 1].0) / 2.0;
+                return mean;
+            }
+            if len < target || len == target && !even {
+                return *v;
+            }
+        }
+        0.0
+    }
+    /// Can be used in [`Self::new`].
+    pub fn split_start(&self, len: usize) -> Vec<(f64, usize)> {
+        let mut sum = 0;
+        let mut list = Vec::new();
+        for (v, count) in self.list {
+            sum += count;
+            if sum >= len {
+                list.push((*v, *count - (sum - len)));
+                break;
+            } else {
+                list.push((*v, *count));
+            }
+        }
+        list
+    }
+    /// Can be used in [`Self::new`].
+    pub fn split_end(&self, len: usize) -> Vec<(f64, usize)> {
+        let len = self.len() - len;
+        let mut sum = self.len();
+        let mut list = Vec::new();
+        for (v, count) in self.list.iter().rev() {
+            sum -= count;
+            if sum <= len {
+                list.insert(0, (*v, *count - (len - sum)));
+                break;
+            } else {
+                list.insert(0, (*v, *count))
+            }
+        }
+        list
+    }
+}
+
+/// Returned from [`std_dev`].
+pub struct MeanOutput {
+    pub standard_deviation: f64,
+    pub mean: f64,
+}
+/// Returned from [`median`]
+pub struct MedianOutput {
+    pub median: f64,
+    pub lower_quadrille: Option<f64>,
+    pub higher_quadrille: Option<f64>,
+}
+
+pub fn std_dev(values: ClusterList) -> MeanOutput {
+    let m = values.sum() / values.len() as f64;
+    let squared_deviations = values.sum_squared_diff(m);
+    let variance: f64 = squared_deviations / (values.len() - 1) as f64;
+    MeanOutput {
+        standard_deviation: variance.sqrt(),
+        mean: m,
+    }
+}
+pub fn median(values: ClusterList) -> MedianOutput {
+    let lower_half = values.split_start(values.len() / 2);
+    let lower_half = ClusterList::new(&lower_half);
+    let upper_half = values.split_end(values.len() / 2);
+    let upper_half = ClusterList::new(&upper_half);
+    MedianOutput {
+        median: values.median(),
+        lower_quadrille: if lower_half.len() > 1 {
+            Some(lower_half.median())
+        } else {
+            None
+        },
+        higher_quadrille: if upper_half.len() > 1 {
+            Some(upper_half.median())
+        } else {
+            None
+        },
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -5,132 +5,10 @@ use std::str::FromStr;
 use std::time::Instant;
 use std::{env, hash};
 
+pub mod lib;
+
 fn f() {
     stdout().lock().flush().unwrap();
-}
-
-struct MultipleList<'a> {
-    list: &'a [(f64, usize)],
-}
-impl<'a> MultipleList<'a> {
-    fn new(list: &'a [(f64, usize)]) -> Self {
-        Self { list }
-    }
-
-    fn len(&self) -> usize {
-        self.list.iter().map(|(_, count)| *count).sum()
-    }
-    fn sum(&self) -> f64 {
-        let mut sum = 0.0;
-        for (v, count) in self.list.iter() {
-            sum += v * *count as f64;
-        }
-        sum
-    }
-    fn sum_squared_diff(&self, base: f64) -> f64 {
-        let mut sum = 0.0;
-        for (v, count) in self.list.iter() {
-            sum += (v - base).powi(2) * *count as f64;
-        }
-        sum
-    }
-    /// The inner list must be sorted by the `f64`.
-    fn median(&self) -> f64 {
-        let len = self.len();
-        let even = len % 2 == 0;
-        let mut len = len;
-        let target = len / 2;
-
-        for (pos, (v, count)) in self.list.iter().enumerate() {
-            len -= *count;
-            if len + 1 == target && even {
-                let mean = (*v + self.list[pos - 1].0) / 2.0;
-                return mean;
-            }
-            if len < target || len == target && !even {
-                return *v;
-            }
-        }
-        0.0
-    }
-    fn split_start(&self, len: usize) -> Vec<(f64, usize)> {
-        let mut sum = 0;
-        let mut list = Vec::new();
-        for (v, count) in self.list {
-            sum += count;
-            if sum >= len {
-                list.push((*v, *count - (sum - len)));
-                break;
-            } else {
-                list.push((*v, *count));
-            }
-        }
-        list
-    }
-    fn split_end(&self, len: usize) -> Vec<(f64, usize)> {
-        let len = self.len() - len;
-        let mut sum = self.len();
-        let mut list = Vec::new();
-        for (v, count) in self.list.iter().rev() {
-            sum -= count;
-            if sum <= len {
-                list.insert(0, (*v, *count - (len - sum)));
-                break;
-            } else {
-                list.insert(0, (*v, *count))
-            }
-        }
-        list
-    }
-}
-
-struct Output {
-    s: f64,
-    m: f64,
-}
-struct MedianOutput {
-    median: f64,
-    lower_quadrille: Option<f64>,
-    higher_quadrille: Option<f64>,
-}
-
-fn std_dev(values: MultipleList) -> Output {
-    let m = values.sum() / values.len() as f64;
-    let squared_deviations = values.sum_squared_diff(m);
-    let variance: f64 = squared_deviations / (values.len() - 1) as f64;
-    Output {
-        s: variance.sqrt(),
-        m,
-    }
-}
-fn median(values: MultipleList) -> MedianOutput {
-    fn median(sorted_values: &[f64]) -> f64 {
-        // even
-        if sorted_values.len() % 2 == 0 {
-            let b = sorted_values.len() / 2;
-            let a = b - 1;
-            (sorted_values[a] + sorted_values[b]) / 2.0
-        } else {
-            sorted_values[sorted_values.len() / 2]
-        }
-    }
-    let lower_half = values.split_start(values.len() / 2);
-    let lower_half = MultipleList::new(&lower_half);
-    let upper_half = values.split_end(values.len() / 2);
-    let upper_half = MultipleList::new(&upper_half);
-    MedianOutput {
-        median: values.median(),
-        lower_quadrille: if lower_half.len() > 1 {
-            Some(lower_half.median())
-        } else {
-            None
-        },
-        higher_quadrille: if upper_half.len() > 1 {
-            Some(upper_half.median())
-        } else {
-            None
-        },
-    }
 }
 
 fn parse<T: FromStr>(s: &str) -> Option<T> {
@@ -226,7 +104,7 @@ fn main() {
         }
         let now = Instant::now();
 
-        let out = std_dev(MultipleList::new(&values));
+        let mean = lib::std_dev(lib::ClusterList::new(&values));
 
         if performance_print {
             println!(
@@ -238,7 +116,7 @@ fn main() {
 
         values.sort_unstable_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
 
-        let median = median(MultipleList::new(&values));
+        let median = lib::median(lib::ClusterList::new(&values));
 
         if performance_print {
             println!("Median & quadrilles took {}µs", now.elapsed().as_micros());
@@ -246,7 +124,7 @@ fn main() {
 
         println!(
             "Standard deviation: {}, mean: {}, median: {}, lower quadrille: {:?}, higher quadrille: {:?}",
-            out.s, out.m, median.median, median.lower_quadrille, median.higher_quadrille,
+            mean.standard_deviation, mean.mean, median.median, median.lower_quadrille, median.higher_quadrille,
         );
     }
 }
