c2447a748dd9c02d4802c1f20cad25120f6863c6
Icelk <main@icelk.dev>
2022-03-13 17:25:25 +0100

    Formatting.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -323,7 +323,8 @@ fn main() {
 
     'main: loop {
         let multiline = {
-            matches.is_present("multiline") || matches!(matches.subcommand_name(), Some("regression"))
+            matches.is_present("multiline")
+                || matches!(matches.subcommand_name(), Some("regression"))
         };
         let input = if let Some(i) = input(tty, debug_performance, multiline, &mut last_prompt) {
             i
